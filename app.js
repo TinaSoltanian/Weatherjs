bcd5f448fb5e03b909da8f889d38181df2d23c12
Tina <tinasoltanian@gmail.com>
2018-06-26 23:53:03 -0700

    hndle save change button click

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const ui = new UI();
 
 document.addEventListener("DOMContentLoaded", getWeather);
 
+//handle change location click
+document.getElementById("w-savechnages").addEventListener("click", (e) =>{
+    const city = document.getElementById("city").value;
+    const state = document.getElementById("state").value;
+
+    weather.changeLocation(city, state);
+
+    //show changed location weather
+    getWeather();
+
+    // close the model
+    $('#locModal').modal('hide');
+})
+
 function getWeather() {
   weather
     .getWeather()
